0e0879b36962015a8d24a2561f4106f01b073fdb
Thiago Augusto <thiago.digitalfront@gmail.com>
2023-01-31 02:21:39 -0300

    [refactory] create card component to refactory

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <article class="uk-article">
+    <img class="uk-margin-remove" :src="data.urlToImage" alt="" />
+    <h4 class="uk-article-title">{{ data.title }}</h4>
+    <p class="uk-article-meta">
+      Escrito por
+      <a href="#" v-if="data.author">{{ data.author }}</a>
+      <a href="#" v-else>Autor desconhecido</a>
+      {{ setDate(data.publishedAt) }}.
+    </p>
+    <p class="uk-article-meta">
+      Publicado em
+      <a :href="data.url">{{ data.source.name }}</a>
+    </p>
+    <p>{{ data.content }}</p>
+  </article>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    data: {
+      type: [Array, Object],
+    },
+  },
+  methods: {
+    setDate(date: Date) {
+      let formatDate = new Date(date);
+      let currentDate = new Intl.DateTimeFormat("pt-BR", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }).format(formatDate);
+      return currentDate;
+    },
+  },
+};
+</script>
--- src/page/ArticlesPage.vue
+++ src/page/ArticlesPage.vue
@@ -1,28 +1,15 @@
 <template>
   <div class="uk-container">
     <div uk-spinner v-if="count == 0">Carregando</div>
-    <div class="uk-grid uk-child-width-1-2" v-else>
+    <div class="uk-grid uk-child-width-1-2" uk-grid v-else>
       <div v-for="(article, index) in articles" :key="index">
-        <article class="uk-article">
-          <img class="uk-margin-remove" :src="article.urlToImage" alt="" />
-          <h4 class="uk-article-title">{{ article.title }}</h4>
-          <p class="uk-article-meta">
-            Escrito por
-            <a href="#" v-if="article.author">{{ article.author }}</a>
-            <a href="#" v-else>Autor desconhecido</a>
-            {{ setDate(article.publishedAt) }}.
-          </p>
-          <p class="uk-article-meta">
-            Publicado em
-            <a :href="article.url">{{ article.source.name }}</a>
-          </p>
-          <p>{{ article.content }}</p>
-        </article>
+        <CardComponent :data="article" />
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
+import CardComponent from "../components/CardComponent.vue";
 import api from "../services/apiService";
 export default {
   data() {
@@ -40,16 +27,9 @@ export default {
       this.count = count;
       console.log(articles);
     },
-    setDate(date: Date) {
-      let formatDate = new Date(date);
-      let currentDate = new Intl.DateTimeFormat("pt-BR", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      }).format(formatDate);
-      return currentDate;
-    },
+  },
+  components: {
+    CardComponent,
   },
   mounted() {
     this.getArticles();
