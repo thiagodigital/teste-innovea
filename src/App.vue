ffa309d23b5526d37937fb2abbea9649b29fd1b5
Thiago Augusto <thiago.digitalfront@gmail.com>
2023-01-31 02:10:18 -0300

    [update] create footer component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <HeaderComponent />
   <RouterView />
+  <FooterComponent />
 </template>
 
 <script lang="ts">
+import FooterComponent from "./components/FooterComponent.vue";
 import HeaderComponent from "./components/HeaderComponent.vue";
 export default {
   components: {
     HeaderComponent,
+    FooterComponent,
   },
 };
 </script>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer
+    class="uk-section uk-section-small uk-section-secondary uk-margin-large-top"
+  >
+    <div class="uk-container">
+      <div class="uk-flex uk-flex-between">
+        <div>
+          <img class="branding" src="../assets/logo.svg" alt="" /> {{ branding
+          }}<b>news</b>
+        </div>
+        <div>
+          <small
+            >Powered by <a href="https://newsapi.org/">newsapi.org</a></small
+          >
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      branding: "",
+    };
+  },
+  mounted() {
+    this.branding = import.meta.env.VITE_SUBJECT;
+  },
+};
+</script>
